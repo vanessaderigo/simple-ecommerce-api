f772c4c7d9536429b3fa6882704daeb23dece832
Vanessa <vderigoesteves@gmail.com>
2025-07-05 22:52:30 -0300

    Initial commit

---
+++ src/main/java/com/vanessa/simpleecommerceapi/SimpleEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.vanessa.simpleecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEcommerceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanessa/simpleecommerceapi/SimpleEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessa.simpleecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
